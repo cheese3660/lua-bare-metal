e532e47c6e5d1533149af45a5a90c3c0568ad55f
velleda <16000407+notvelleda@users.noreply.github.com>
2023-10-01 02:47:02 -0500

    i forgor

--- Makefile
+++ Makefile
@@ -1,20 +1,28 @@
 LDFLAGS += -melf_i386 -Tkernel.ld
-CFLAGS += -O2 -m32 -march=i386 -nostartfiles -nostdlib -static
+CFLAGS += -O2 -m32 -march=i386 -nostartfiles -nostdlib -nostdinc -fno-stack-protector -static -static-libgcc \
+	-Ilibc/include -Ilibc/printf/src -Ilibc/arch/x86/include -Ilibc/openlibm/include -Ilibc/openlibm/src \
+	-Ilua -include common.h
 ASFLAGS += -32 -march=i386
 
-OBJECTS = init.o main.o
+LUA_OBJS = lua/lapi.o lua/lcode.o lua/lctype.o lua/ldebug.o lua/ldo.o lua/ldump.o lua/lfunc.o lua/lgc.o lua/llex.o \
+	lua/lmem.o lua/lobject.o lua/lopcodes.o lua/lparser.o lua/lstate.o lua/lstring.o lua/ltable.o \
+	lua/ltm.o lua/lundump.o lua/lvm.o lua/lzio.o lua/ltests.o lua/lauxlib.o lua/lbaselib.o
+
+MATH_OBJS = libc/openlibm/src/e_fmod.o libc/openlibm/src/e_pow.o libc/openlibm/src/s_frexp.o libc/openlibm/src/s_scalbn.o libc/openlibm/src/e_sqrtf.o
+
+OBJECTS = init.o main.o $(LUA_OBJS) arith64/arith64.o $(MATH_OBJS)
 BINARY = kernel
 
 all: $(BINARY)
 
 $(BINARY): $(OBJECTS)
-	$(LD) $(LDFLAGS) $(OBJECTS) -o $(BINARY)
+	$(LD) $(LDFLAGS) $(OBJECTS) libc/buildresults/src/libc.a -o $(BINARY)
 
 %.o: %.S
 	$(AS) $(ASFLAGS) $< -o $@
 
 %.o: %.c
-	$(CC) $(CFLAGS) -c $<
+	$(CC) $(CFLAGS) -c $< -o $@
 
 .PHONY: clean
 clean:
--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+#include <time.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
 static inline unsigned char inb(unsigned short addr) {
     unsigned char result;
 
@@ -18,18 +25,129 @@ static inline void outb(unsigned short addr, unsigned char value) {
     );
 }
 
-static void serial_putb(unsigned char b) {
+void putchar_(char c) {
     while ((inb(0x3f8 + 5) & 0x20) == 0);
-    outb(0x3f8, b);
-    outb(0xe9, b);
+    outb(0x3f8, c);
+    outb(0xe9, c);
 }
 
-static void serial_puts(char *str) {
-    while (*str)
-        serial_putb(*str ++);
+void write_string(const char *string, int len) {
+    for (; len; len --)
+        putchar_(*string ++);
+}
+
+time_t time(time_t*) {
+    return 0;
+}
+
+/* TODO: steal this from somewhere better than GCC */
+uint64_t __udivmoddi4(uint64_t num, uint64_t den, uint64_t *rem_p) {
+    uint64_t quot = 0, qbit = 1;
+
+    if (den == 0) {
+        return 1 / ((unsigned) den); /* Intentional divide by zero, without
+                                    triggering a compiler warning which
+                                    would abort the build */
+    }
+
+    /* Left-justify denominator and count shift */
+    while ((int64_t) den >= 0) {
+        den <<= 1;
+        qbit <<= 1;
+    }
+
+    while (qbit) {
+        if (den <= num) {
+            num -= den;
+            quot += qbit;
+        }
+        den >>= 1;
+        qbit >>= 1;
+    }
+
+    if (rem_p)
+        *rem_p = num;
+
+    return quot;
+}
+
+int errno = 0;
+
+int feof(FILE*) {
+    errno = 1;
+    return 0;
+}
+
+size_t fread(void* restrict, size_t, size_t, FILE* restrict) {
+    errno = 1;
+    return 0;
+}
+
+int fputs(const char* restrict, FILE* restrict) {
+    errno = 1;
+    return 0;
+}
+
+int fflush(FILE *) {
+    errno = 1;
+    return 0;
+}
+
+size_t fwrite(const void* __restrict, size_t, size_t, FILE* __restrict) {
+    errno = 1;
+    return 0;
+}
+
+int getc(FILE*) {
+    errno = 1;
+    return 0;
+}
+
+FILE* fopen(const char* __restrict, const char* __restrict) {
+    errno = 1;
+    return NULL;
+}
+
+int ferror(FILE*) {
+    errno = 1;
+    return 0;
+}
+
+int fclose(FILE*) {
+    errno = 1;
+    return 0;
+}
+
+FILE* freopen(const char* __restrict, const char* __restrict, FILE* __restrict) {
+    errno = 1;
+    return NULL;
+}
+
+void *malloc(size_t size) {
+    printf("malloc called!\n");
+    return NULL;
+}
+
+void free(void*) {
+    printf("free called!\n");
 }
 
 void kmain(void) {
-    serial_puts(":3c\n");
+    const char *buff = "print(\"UwU OwO\")\n";
+    int error;
+    const luaL_Reg *lib;
+    lua_State *L = luaL_newstate();
+
+    luaL_requiref(L, LUA_GNAME, luaopen_base, 1);
+    lua_pop(L, 1);
+
+    error = luaL_loadbuffer(L, buff, strlen(buff), "line") || lua_pcall(L, 0, 0, 0);
+    if (error) {
+        printf("%s", lua_tostring(L, -1));
+        lua_pop(L, 1);  /* pop error message from the stack */
+    }
+
+    lua_close(L);
+
     while (1);
 }
