1d61c18676060d4c631a432057fe7e7f789d1d85
Lexi Allen <arallen3660@gmail.com>
2023-10-05 19:37:27 -0400

    Work on starting to use grub

--- run.sh
+++ run.sh
@@ -2,5 +2,18 @@
 
 set -e
 make
+mkdir -p iso
+mkdir -p iso/boot
+mkdir -p iso/boot/grub
+cp kernel ./iso/boot/kernel
+cp $1 iso/boot/initrd
+cat > iso/boot/grub/grub.cfg << EOF
+menuentry "openos" {
+    multiboot /boot/kernel
+    initrd /boot/initrd
+}
+EOF
+grub-mkrescue -o kernel.iso iso
 echo "(ctrl+c to exit)"
-qemu-system-i386 -cpu 486 -device isa-debug-exit -kernel kernel -serial stdio -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0 $@
+
+qemu-system-i386 -cpu 486 -device isa-debug-exit -cdrom kernel.iso -serial stdio -audiodev pa,id=snd0 -machine pcspk-audiodev=snd0
